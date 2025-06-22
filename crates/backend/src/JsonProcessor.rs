78caf8132b809ab4152b5d60853bfc8f75f3912b
Gerorge Liao <george.liao@canonical.com>
2025-06-22 21:42:58 +0200

    [jsonprocessor] added scaffold of the public methods.

--- crates/backend/src/JsonProcessor.rs
+++ crates/backend/src/JsonProcessor.rs
@@ -2,13 +2,27 @@ use crate::downloader;
 
 // #[derive(Default)]
 struct JsonProcessor {
-    json_content : String,
+    json_content: String,
 }
 
 impl JsonProcessor {
     pub fn new() -> Self {
         //TODO replace unwrap to ? later maybe
-        return JsonProcessor {json_content : downloader::linear_download().unwrap()};
+        return JsonProcessor {
+            json_content: downloader::linear_download().unwrap(),
+        };
+    }
+
+    pub fn get_supported_ubuntu_releases() -> Vec<String> {
+        return vec![];
+    }
+
+    pub fn get_current_ubuntu_lts() -> String {
+        return String::new();
+    }
+
+    pub fn get_sha256(ubuntu_release : String) -> String {
+        return String::new();
     }
 }
 
