71addc1d95021c08092c36bec5f2b1399050df55
Gerorge Liao <george.liao@canonical.com>
2025-06-22 18:17:31 +0200

    [jsonProcessor] added json struct.

--- crates/backend/src/JsonProcessor.rs
+++ crates/backend/src/JsonProcessor.rs
@@ -1,5 +1,23 @@
-// use crate::downloader;
+use crate::downloader;
 
-struct JsonProcessor
-{
+// #[derive(Default)]
+struct JsonProcessor {
+    json_content : String,
+}
+
+impl JsonProcessor {
+    pub fn new() -> Self {
+        //TODO replace unwrap to ? later maybe
+        return JsonProcessor {json_content : downloader::linear_download().unwrap()};
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_linear_download() {
+        let processor = JsonProcessor::new();
+    }
 }
