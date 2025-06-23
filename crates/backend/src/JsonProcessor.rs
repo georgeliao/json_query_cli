0d553033441694070a834d6b923b1e3d4e2dbfde
Gerorge Liao <george.liao@canonical.com>
2025-06-23 15:38:27 +0200

    [json processor] added the get_current_ubuntu_lts implementation and unit test.

--- crates/backend/src/JsonProcessor.rs
+++ crates/backend/src/JsonProcessor.rs
@@ -1,27 +1,51 @@
 use crate::downloader;
-
-// #[derive(Default)]
+use serde_json::Value;
 struct JsonProcessor {
-    json_content: String,
+    json_content_parsed: Value,
 }
 
 impl JsonProcessor {
     pub fn new() -> Self {
         //TODO replace unwrap to ? later maybe
         return JsonProcessor {
-            json_content: downloader::linear_download().unwrap(),
+            json_content_parsed: serde_json::from_str(&downloader::linear_download().unwrap())
+                .unwrap(),
         };
     }
 
-    pub fn get_supported_ubuntu_releases() -> Vec<String> {
-        return vec![];
+    pub fn get_supported_ubuntu_releases(&self) -> Vec<String> {
+        return vec![String::from("")];
     }
 
-    pub fn get_current_ubuntu_lts() -> String {
-        return String::new();
+    pub fn get_current_ubuntu_lts(&self) -> f64 {
+        let mut current_version = 0.0;
+
+        let products = self
+            .json_content_parsed
+            .get("products")
+            .unwrap()
+            .as_object()
+            .unwrap();
+        for (key, val) in products {
+            if key.ends_with("amd64") {
+                let release_title_value: &str = val.get("release_title").unwrap().as_str().unwrap();
+                if release_title_value.ends_with("LTS") {
+                    let version_value = val
+                        .get("version")
+                        .unwrap()
+                        .as_str()
+                        .unwrap()
+                        .parse::<f64>()
+                        .unwrap();
+                    current_version = f64::max(current_version, version_value);
+                }
+            }
+        }
+
+        return current_version;
     }
 
-    pub fn get_sha256(ubuntu_release : String) -> String {
+    pub fn get_sha256(&self, ubuntu_release: String) -> String {
         return String::new();
     }
 }
@@ -31,7 +55,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_linear_download() {
+    fn test_get_current_ubuntu_lts() {
         let processor = JsonProcessor::new();
+        assert_eq!(processor.get_current_ubuntu_lts(), 24.04);
     }
 }
