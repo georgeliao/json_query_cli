119e8f194b15e6e9affc439ae299c072ec7db913
Gerorge Liao <george.liao@canonical.com>
2025-06-24 13:17:06 +0200

    [main] setup the cli interface.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
-use clap::{Parser};
+use clap::{ArgGroup, Parser};
 
 #[derive(Parser)]
 #[command(name = "json_cli", version = "1.0", author = "George Liao")]
+#[command(
+    about = "Query Ubuntu cloud image info",
+    group(
+        ArgGroup::new("mode")
+            .args(&["lts", "releases", "sha256_release"])
+            .required(true)
+            .multiple(false) // ensures only one can be used
+    )
+)]
 struct Cli {
-    /// Activate verbose mode
     #[arg(short, long)]
-    verbose: bool,
-
-    /// Input arg
+    lts: bool,
+    /// get supported ubuntu releases
     #[arg(short, long)]
-    input: String,
+    releases: bool,
+    /// get the shar256 of a disk1_img of a release
+    #[arg(short, long)]
+    sha256_release: Option<String>,
 }
 
 fn main() {
-    // let cli = Cli::parse();
     let cli: Cli = Cli::parse();
-    if cli.verbose {
-        println!("Verbose mode is on");
+
+    if cli.lts {
+        println!("Getting current LTS...");
+    } else if cli.releases {
+        println!("Listing all supported releases...");
+    } else if let Some(release) = cli.sha256_release {
+        println!("Getting sha256 for release: {release}");
+    } else {
+        eprintln!("No valid mode specified.");
+        std::process::exit(1);
     }
-    println!("Input arg: {}", cli.input);
 }
