393673a3771ac235782d070ddce9bac1f740c54a
Gerorge Liao <george.liao@canonical.com>
2025-06-24 11:55:37 +0200

    [JsonProcessor] Refined the jsonprocessor constructor and get_supported_ubuntu_releases function.

--- crates/backend/src/JsonProcessor.rs
+++ crates/backend/src/JsonProcessor.rs
@@ -1,39 +1,32 @@
 use crate::downloader;
 use serde_json::Value;
+
 struct JsonProcessor {
     json_content_parsed: Value,
 }
+// pub type Result<T> = std::result::Result<T, Error>;
 
 impl JsonProcessor {
-    pub fn new() -> Self {
-        //TODO replace unwrap to ? later maybe
-        return JsonProcessor {
-            json_content_parsed: serde_json::from_str(&downloader::linear_download().unwrap())
-                .unwrap(),
-        };
+    pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        return Ok(JsonProcessor {
+            json_content_parsed: serde_json::from_str(&downloader::linear_download()?)?,
+        });
     }
 
-    pub fn get_supported_ubuntu_releases(&self) -> Vec<String> {
+    pub fn get_supported_ubuntu_releases(&self) -> Option<Vec<String>> {
         let mut release_strs: Vec<String> = vec![];
-        let products = self
-            .json_content_parsed
-            .get("products")
-            .unwrap()
-            .as_object()
-            .unwrap();
+        let products = self.json_content_parsed.get("products")?.as_object()?;
         for (product_name, val) in products {
             if product_name.ends_with("amd64") {
-                let supported = val.get("supported").unwrap().as_bool().unwrap();
+                let supported = val.get("supported")?.as_bool()?;
                 if (supported) {
-                    let version = val.get("version").unwrap().as_str().unwrap();
+                    let version = val.get("version")?.as_str()?;
                     release_strs.push(version.to_owned());
                 }
             }
         }
-        for (ele) in &release_strs {
-            println!("{}", ele);
-        }
-        return release_strs;
+
+        return Some(release_strs);
     }
 
     pub fn get_current_ubuntu_lts(&self) -> Option<f64> {
@@ -78,20 +71,20 @@ mod tests {
 
     #[test]
     fn test_get_current_ubuntu_lts() {
-        let processor = JsonProcessor::new();
+        let processor = JsonProcessor::new().unwrap();
         assert_eq!(processor.get_current_ubuntu_lts().unwrap(), 24.04);
     }
 
     #[test]
     fn test_get_supported_ubuntu_releases() {
-        let processor = JsonProcessor::new();
+        let processor = JsonProcessor::new().unwrap();
         processor.get_supported_ubuntu_releases();
     }
 
     #[test]
     fn test_get_disk1_img_sha256_of_release() {
-        let processor = JsonProcessor::new();
-        let sha256 = processor
+        let processor = JsonProcessor::new().unwrap();
+        let sha256: String = processor
             .get_disk1_img_sha256_of_release("com.ubuntu.cloud:server:21.10:amd64", "20220708")
             .unwrap();
         println!("The sha256 value is {}", sha256);
