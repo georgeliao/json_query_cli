b480a3ec55ce50cfe139cbdcbcf6353ff01b1520
Gerorge Liao <george.liao@canonical.com>
2025-06-24 17:31:45 +0200

    [lib.rs] fixed format.

--- crates/backend/src/lib.rs
+++ crates/backend/src/lib.rs
@@ -1,5 +1,5 @@
 pub mod downloader;
 pub mod json_processor;
 pub mod ubuntu_image_json_processor;
-pub use ubuntu_image_json_processor::UbuntuImageJsonProcessor;
 pub use json_processor::JsonProcessor;
+pub use ubuntu_image_json_processor::UbuntuImageJsonProcessor;
