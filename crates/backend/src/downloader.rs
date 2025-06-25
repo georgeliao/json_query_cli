d42316c4c7e94a6811e4fab2e34629d719d59ae5
Gerorge Liao <george.liao@canonical.com>
2025-06-25 18:22:09 +0200

    [downloader] changed the runtime const variable to a compile time constant variable.

--- crates/backend/src/downloader.rs
+++ crates/backend/src/downloader.rs
@@ -2,8 +2,8 @@ use std::error;
 
 pub fn linear_download() -> Result<String, Box<dyn error::Error>> {
     // TODO, mock the downloader to facilitate unit testing.
-    let download_url: &str = "https://cloud-images.ubuntu.com/releases/streams/v1/com.ubuntu.cloud:released:download.json";
-    let response: String = reqwest::blocking::get(download_url)?.text()?;
+    const URL: &str = "https://cloud-images.ubuntu.com/releases/streams/v1/com.ubuntu.cloud:released:download.json";
+    let response: String = reqwest::blocking::get(URL)?.text()?;
 
     return Ok(response);
 }
